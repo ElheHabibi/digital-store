3e7125f6b943a06f931d2b79be743e1d0309bdde
Elahe Habibi <elhe.habibi@gmail.com>
2026-08-03 03:29:32 +0330

    feat: create products page

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import ProductItem from "../../components/productItem/ProductItem";
+import Container from "../../components/container/Container";
 
 function Store() {
   return (
-    <div>
-      store
-    </div>
-  )
+      <div className="p-4 bg-gray-200">
+    <Container >
+      <h1 className="text-right mt-5 mb-4">صفحه محصولات</h1>
+      <div className="grid grid-cols-4 gap-4 ">
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+      </div>
+    </Container>
+      </div>
+  );
 }
 
-export default Store
+export default Store;
