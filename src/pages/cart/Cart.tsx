1e8a5d11a146e69b229bee489bcea97f0e1de1ab
Elahe Habibi <elhe.habibi@gmail.com>
2026-08-05 14:34:06 +0330

    feat: add cart-page

---
+++ src/pages/cart/Cart.tsx
@@ -0,0 +1,36 @@
+import Button from "../../components/button/Button";
+import CartItem from "../../components/cartItem/CartItem";
+import Container from "../../components/container/Container";
+
+function Cart() {
+  return (
+    <Container>
+      <div className=" mt-10 gap-4">
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+
+
+      <div className=" flex justify-between items-center flex-row-reverse bg-gray-100 rounded p-4 mt-20 mb-20">
+
+      <div className="flex flex-col gap-4 text-right">
+        <p>قیمت کل: 500000 تومان</p>
+        <p>تخفیف: 100000 تومان</p>
+        <p>قیمت نهایی: 400000 تومان</p>
+
+      </div>
+      <div className="py-2 px-4">
+        <Button>
+          ثبت سفارش
+        </Button>
+        </div>        
+
+      </div>
+    </Container>
+  );
+}
+
+export default Cart;
